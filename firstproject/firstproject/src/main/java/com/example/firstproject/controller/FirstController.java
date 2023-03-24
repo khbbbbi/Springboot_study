6719121393d4ece7a3a636571b526d00562a80fe
khbbbbi <gkaqk0626@naver.com>
2023-03-24 09:58:08 +0900

    한글 깨짐 현상 설정

--- firstproject/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -9,7 +9,7 @@ public class FirstController {
     @GetMapping("/hi")
     public String niceToMeetYou(Model model){
         //변수 등록
-        model.addAttribute("username", "hanbi");
+        model.addAttribute("username", "감자");
         return "greetings"; //templates안에 greetings.mustache -> 브라우저로 전송
     }
     @GetMapping("/bye")
