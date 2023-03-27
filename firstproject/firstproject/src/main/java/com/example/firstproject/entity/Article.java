a161321a7d45ceb3d0aceecdc162260adeed8c2b
khbbbbi <gkaqk0626@naver.com>
2023-03-28 00:21:49 +0900

    롬복과 로깅

--- firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,38 +1,40 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
-@Entity //DB가 해당 객체를 인식 가능
+@Entity //DB가 해당 객체를 인식 가능!
+@AllArgsConstructor //생성자 대신
+@ToString
 public class Article {
-
     @Id //대표값을 지정! like a 주민등록번호
-    @GeneratedValue //1, 2, 3, ....자동 생성 어노테이션!
+    @GeneratedValue     // 1, 2, 3, ... 자동 생성 어노테이션!
     private Long id;
+
     @Column
     private String title;
+
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+    public Article() {}
 
-//    public Article() {
-//
+//    public Article(Long id, String title, String content) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
 //    }
 
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Article{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 }
